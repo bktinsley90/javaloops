3d905aa72b8719d78020e84e7d8b034850329028
Brittany Tinsley <brittanytinsley@Brittanys-MBP.jetpack>
2020-03-24 21:51:54 -0400

    Completed all excerises

---
+++ scripts.js
@@ -0,0 +1,30 @@
+
+    //    Make a loop that counts from 0 to 10.  It should print out in uppercase, "The number is" and each integer, from 0 to 10, in the Console.  Make sure to have the right spacing in the sentence.
+    //     for (let i = 0; i < 10; i++) {
+    //        let message = `The Number is ${i}`;
+    //        console.log(message.toUpperCase())
+    //    } 
+    // Make a loop that counts from 5 to 15 in the Console, in multiples of 3.  
+
+    //    for (let i = 6; i <= 15; i+=3){
+    //     console.log(i)
+    //    }
+    // Make a loop that counts by two, from 40 to 60. Alert the results with each integer and then the following text, "little monkeys sitting in a tree" in lowercase. Make sure to have the right spacing in the sentence and add line breaks between lines (add the string "\n" in between each value)
+    
+    //    for (let i = 40; i<= 60; i += 2) {
+    //        alert(` ${i} + lttle monkeys, siting in a tree`.toLowerCase() + `\n`);
+    //    }
+
+    // Make a loop that counts backwards from 100 to 0, and prints out in the Console.
+
+    // for (let i = 100; i >= 0; i--) {
+    //     console.log (`${i}`);
+    // }
+    
+    // Make a loop that repeats a phrase a certain amount of times. Declare a variable called love and have it be a string that says, "Boston CodeSquad". Use the length of love to determine how many times this phrase should repeat. The phase should say, "I love JavaScript!" repeatedly
+
+    let love = "Boston CodeSquad"
+    let width = love.length;
+    for (i = 0; i <= width; i++ ) {
+        console.log(`I love JavaScript!`)
+    }
